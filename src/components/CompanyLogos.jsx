a597603569415585c371afeef913bc08b5d30c9b
RafiNazhmiNugraha <rafinazhminugraha@upi.edu>
2025-11-22 19:01:29 +0700

    change the source

--- src/components/CompanyLogos.jsx
+++ src/components/CompanyLogos.jsx
@@ -1,4 +1,5 @@
-import { companyLogos } from "../constants";
+import RafiNazhmiLogo from "../assets/RAFI NAZHMI.png";
+const companyLogos = Array(6).fill(RafiNazhmiLogo); // duplicate logo to show multiple
 
 const CompanyLogos = ({ className }) => {
   return (
@@ -6,13 +7,13 @@ const CompanyLogos = ({ className }) => {
       <h5 className="tagline mb-6 text-center text-n-1/50">
         Helping people create beautiful content at
       </h5>
-      <ul className="flex">
+      <ul className="flex flex-nowrap justify-center gap-10 overflow-x-auto">
         {companyLogos.map((logo, index) => (
           <li
-            className="flex items-center justify-center flex-1 h-[8.5rem]"
+            className="flex items-center justify-center h-[10rem]"
             key={index}
           >
-            <img src={logo} width={134} height={28} alt={logo} />
+            <img src={logo} width={250} height={50} alt={`RAFI NAZHMI ${index + 1}`} />
           </li>
         ))}
       </ul>
